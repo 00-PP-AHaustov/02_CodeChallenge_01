d5a43b87ca936010801906fe3b2fa29923b8bb12
Anton <anton.haust@gmail.com>
2023-07-13 12:55:02 +0200

    challenge

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,11 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+"use strict"
+
+let a,b,c ;
+a = prompt("Geben Sie bitte die erste Zahl ein: ");
+b = prompt("Geben Sie bitte die zweite Zahl ein: ");
+c = a+b;
+
+console.log("Die Summe der Zahlen "+a+" und "+b+" ist: "+c);
